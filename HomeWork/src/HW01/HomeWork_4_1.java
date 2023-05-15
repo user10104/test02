3f6eb49b1e34b9b7250b5ccea1bb7821b3ab0197
user10104 <adadax112@gmail.com>
2023-05-15 18:03:31 +0900

    modified

--- HomeWork/src/HW01/HomeWork_4_1.java
+++ HomeWork/src/HW01/HomeWork_4_1.java
@@ -16,3 +16,14 @@ public class HomeWork_4_1 {
 
 }
 	}
+
+/*
+ * 2.char 형 변수ch 가 공백이나 탭이 아닐 때 true인 조건식
+ * 3.char 형 변수ch 가 'x' 또는  'X'일때 true인 조건식
+ * 4.char 형 변수ch 가 숫자 ('0'~'9')일때 true인 조건식
+ * 5.char 형 변수ch 가 영문자(대문자 또는 소문자 일때 true인 조건식
+ * 6.int 형 변수year 가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지 않을 때 true인 조건식
+ * 7.boolean형 변수 powerOn 가 false일때 true인 조건식
+ * 8.문자열 참조변수 str이 "yes"일때 true인 조건식
+ */
+ 
